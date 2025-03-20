347dccf9fba181b1d09355b784d7063c861d30f7
Adam Tupper <adam.tupper.1@ulaval.ca>
2025-03-20 18:57:59 -0400

    Fix filename.

--- scripts/slurm/launch_model_arch_trivial_augment_evaluations.sh
+++ scripts/slurm/launch_model_arch_trivial_augment_evaluations.sh
@@ -7,7 +7,7 @@
 
 # sbatch --time=32:00:00 --job-name=camus_classification usaugment-experiments/scripts/slurm/train_trivial_augment.sh camus_v4 camus_classification mitb2_multiclass 0.00001 0.000001
 # sbatch --time=32:00:00 --job-name=camus_classification usaugment-experiments/scripts/slurm/train_trivial_augment.sh camus_v4 camus_classification efficientnetb5_multiclass 0.00001 0.000001
-sbatch --time=48:00:00 --job-name=camus_segmentation usaugment-experiments/scripts/slurm/train_trivial_augment.sh camus_v4 camus_segmentation segformerb2_multiclass 0.0000464159 0.00001
+# sbatch --time=48:00:00 --job-name=camus_segmentation usaugment-experiments/scripts/slurm/train_trivial_augment.sh camus_v4 camus_segmentation segformerb2_multiclass 0.0000464159 0.00001
 sbatch --time=48:00:00 --job-name=camus_segmentation usaugment-experiments/scripts/slurm/train_trivial_augment.sh camus_v4 camus_segmentation efficientnetb5_unet_multiclass 0.000215443 0.0000316228
 
 # sbatch --time=4:00:00 --job-name=mmotu_classification usaugment-experiments/scripts/slurm/train_trivial_augment.sh mmotu_v2 mmotu_classification mitb2_multiclass 0.000215443 0.0001
